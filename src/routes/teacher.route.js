8be205ba841fc37491e05a3282e90d3f917bf3cf
Daniduf2001 <danidufernando2002@gmail.com>
2023-03-02 10:58:19 +0530

    create teacher route

---
+++ src/routes/teacher.route.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', async (req, res, next) => {
+    res.send({message: 'Ok teacher api is working 🚀'});
+});
+
+router.post("/", async (req, res, next) => {
+    const {email, password} = req.body;
+    if (email === "" || password === "") {
+        res.status(400).send({message: "Email or password is missing"});
+    }
+});
+
+router.put("/", async (req, res, next) => {
+    const {email, password} = req.body;
+    if (email === "" || password === "") {
+        res.status(400).send({message: "Email or password is missing"});
+    }
+});
+
+router.delete("/", async (req, res, next) => {
+    const {email, password} = req.body;
+    if (email === "" || password === "") {
+        res.status(400).send({message: "Email or password is missing"});
+    }
+});
+
+
+router.get('/:id', async (req, res, next) => {
+    const {id} = req.params;
+    res.send({message: `Ok api is working with ${id} 🚀`});
+});
+
+
+module.exports = router;
