70721071e9f1a233f289f93c8ec2d65cd7c25584
Daniduf2001 <danidufernando2002@gmail.com>
2023-03-02 10:32:00 +0530

    implemented student model

---
+++ src/model/Student.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const StudentSchema = new mongoose.Schema({
+        StudentName: {
+            type: String,
+            required: true,
+        },
+        StudentMobile: {
+            type: String,
+            required: true,
+        },
+        StudentEmailAddress: {
+            type: String,
+            required: true,
+        },
+        EducationalInstitute: {
+            type: String,
+            required: true,
+        },
+    }, {timestamps: true}
+);
+
+const StudentModel = mongoose.model("Student", StudentSchema);
+
+module.exports = StudentModel;
